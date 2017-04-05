63fc95f9e159b284828d948167e69a82a768da11
UniquelySimilar <consciousexplorer@gmail.com>
2017-04-05 16:54:21 -0600

    began customer api store impl

--- src/main/java/com/tcoveney/controller/CustomerApiController.java
+++ src/main/java/com/tcoveney/controller/CustomerApiController.java
@@ -5,8 +5,16 @@ import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +22,7 @@ import com.tcoveney.dao.CustomerDao;
 import com.tcoveney.model.Customer;
 
 @RestController
-@RequestMapping("/customer/api")
+@RequestMapping("/api/customer")
 public class CustomerApiController {
 	private Log log = LogFactory.getLog(CustomerController.class);
 	
@@ -29,5 +37,21 @@ public class CustomerApiController {
 
 		return customerList;
     }
+	
+	@PostMapping("/store")
+	public ResponseEntity<String> store(@Validated @ModelAttribute Customer customer) {
+		log.info("Called CustomerApiController.store()");
+		
+		// TODO: Determine how to use existing validation here. Possibly same as regular controller.
+		
+		log.info("Customer name: " + customer.getName());
+		
+//		int primaryKey = customerDao.insert(customer);
+//		log.info("New customer primary key: " + primaryKey);
+		
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.set("MyResponseHeader", "MyValue");
+		return new ResponseEntity<String>("New customer created", responseHeaders, HttpStatus.CREATED);
+	}
 
 }
