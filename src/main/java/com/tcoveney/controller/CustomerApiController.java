a4be06e7168d6516dc37154c324fe29b59f12dcb
UniquelySimilar <consciousexplorer@gmail.com>
2017-05-12 15:06:12 -0600

    finished impl of rest api customer edit

--- src/main/java/com/tcoveney/controller/CustomerApiController.java
+++ src/main/java/com/tcoveney/controller/CustomerApiController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,7 +41,7 @@ public class CustomerApiController {
 	
 	@GetMapping("")
     public List<Customer> index(Model model) {
-		log.info("Called CustomerApiController.index()");
+		//log.info("Called CustomerApiController.index()");
 		
 		List<Customer> customerList = customerDao.findAll();
 
@@ -49,7 +50,7 @@ public class CustomerApiController {
 	
 	@GetMapping("/{id}")
     public Customer index(@PathVariable("id") int id) {
-		log.info("Called CustomerApiController.show()");
+		//log.info("Called CustomerApiController.show()");
 		
 		Customer customer = customerDao.find(id);
 
@@ -60,8 +61,8 @@ public class CustomerApiController {
 	public ResponseEntity<String> store(@Validated Customer customer, BindingResult result) {
 		// NOTE: When the request Content-Type header is set to 'application/x-www-form-urlencoded', annotating
 		// the Customer parameter with @RequestBody was resulting in an HTTP status code 415 - Unsupported Media Type
-		log.info("Called CustomerApiController.store()");
-
+		//log.info("Called CustomerApiController.store()");
+		//log.info("Customer: " + customer.toString());
 		HttpHeaders responseHeaders = new HttpHeaders();
 		
 		if (result.hasErrors()) {
@@ -71,12 +72,28 @@ public class CustomerApiController {
 			return new ResponseEntity<String>("ERROR validating new Customer", responseHeaders, HttpStatus.BAD_REQUEST);
 		}
 		
-		log.info("Customer: " + customer.toString());
 		
 		int primaryKey = customerDao.insert(customer);
 		log.info("New customer primary key: " + primaryKey);
 		
 		return new ResponseEntity<String>("New customer created", responseHeaders, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/update")
+	public ResponseEntity<String> update(@Validated Customer customer, BindingResult result) {
+		//log.info("Called CustomerApiController.update()");
+		//log.info("Customer: " + customer.toString());
+		
+		if (result.hasErrors()) {
+			log.warn("Updated Customer contains validation error");
+			// TODO: Determine how to get specific validation error message
+			//log.warn(result.getFieldError().toString());
+			return new ResponseEntity<String>("ERROR validating Customer update", HttpStatus.BAD_REQUEST);
+		}
+		
+		customerDao.update(customer);
+		
+		return new ResponseEntity<String>(HttpStatus.NO_CONTENT);
 	}
 
 }
